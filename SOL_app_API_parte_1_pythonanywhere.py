66dc1b0da457b225be580741c9a6753903ec4120
Belen Zubillaga <zubillaga_belen@hotmail.com>
2023-02-18 12:33:23 +0100

    Update SOL_app_API_parte_1_pythonanywhere.py

--- SOL_app_API_parte_1_pythonanywhere.py
+++ SOL_app_API_parte_1_pythonanywhere.py
@@ -8,6 +8,7 @@ import numpy as np
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+
 root = '/home/arzbzub/Aplication_API/modelo/'
 root_db = '/home/arzbzub/Aplication_API/databases/'
 
@@ -15,7 +16,7 @@ model = pickle.load(open(root + 'advertising.model', 'rb'))
 print(model.coef_)
 
 # POST {"TV":, "radio":, "newspaper":} -> It returns the sales prediction for input investments
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST', 'GET'])
 def get_predict():
 
     # Get current time for the PREDICTIONS table
@@ -27,6 +28,8 @@ def get_predict():
 
     # Get POST JSON data
     data = request.get_json()
+    if data == None:
+        data = request.args
     tv = data.get("TV",0)
     radio = data.get("radio",0)
     newspaper = data.get("newspaper",0)
